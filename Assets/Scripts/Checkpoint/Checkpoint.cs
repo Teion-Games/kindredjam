0e83d32f165e8e65f00a86b74422a589569fdd9a
Breno Dória <31598519+Breno-D@users.noreply.github.com>
2021-04-10 11:47:26 -0300

    [RWK] checkpoint

--- Assets/Scripts/Checkpoint/Checkpoint.cs
+++ Assets/Scripts/Checkpoint/Checkpoint.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class Checkpoint : MonoBehaviour
 {
     public Vector2 p1SavePosition, p2SavePosition;
+    public bool saveP1Pos, saveP2Pos;
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.tag=="Player1" || other.gameObject.tag=="Player2")
         {
-            PlayerPrefs.SetFloat("p1Xpos", p1SavePosition.x);
-            PlayerPrefs.SetFloat("p1Ypos", p1SavePosition.y);
-            PlayerPrefs.SetFloat("p2Xpos", p2SavePosition.x);
-            PlayerPrefs.SetFloat("p2Ypos", p2SavePosition.y);
+            if(saveP1Pos)
+            {
+                PlayerPrefs.SetFloat("p1Xpos", p1SavePosition.x);
+                PlayerPrefs.SetFloat("p1Ypos", p1SavePosition.y);
+            }
+            if(saveP2Pos)
+            {
+                PlayerPrefs.SetFloat("p2Xpos", p2SavePosition.x);
+                PlayerPrefs.SetFloat("p2Ypos", p2SavePosition.y);
+            }
         }
     }
 }
