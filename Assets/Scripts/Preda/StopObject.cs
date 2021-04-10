0c574974718bd3de1de422b03a21ae786ebf600f
Breno Dória <31598519+Breno-D@users.noreply.github.com>
2021-04-10 00:15:56 -0300

    [ADD] stopobject

--- Assets/Scripts/Preda/StopObject.cs
+++ Assets/Scripts/Preda/StopObject.cs
@@ -8,7 +8,14 @@ public class StopObject : MonoBehaviour
     {
         if(other.gameObject.tag == "Player1" || other.gameObject.tag == "Player2")
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0f,0f);
+            GetComponent<Rigidbody2D>().isKinematic = true;
+        }
+    }
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Player1" || other.gameObject.tag == "Player2")
+        {
+            GetComponent<Rigidbody2D>().isKinematic = false;
         }
     }
 }
