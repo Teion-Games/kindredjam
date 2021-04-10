b3d367a25d84af6cc4d8de66057556a4cc74ea3a
Breno Dória <31598519+Breno-D@users.noreply.github.com>
2021-04-10 00:22:31 -0300

    [add] unstopabble force

--- Assets/Scripts/Preda/StopObject.cs
+++ Assets/Scripts/Preda/StopObject.cs
@@ -8,14 +8,14 @@ public class StopObject : MonoBehaviour
     {
         if(other.gameObject.tag == "Player1" || other.gameObject.tag == "Player2")
         {
-            GetComponent<Rigidbody2D>().isKinematic = true;
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
         }
     }
     void OnCollisionExit2D(Collision2D other)
     {
         if(other.gameObject.tag == "Player1" || other.gameObject.tag == "Player2")
         {
-            GetComponent<Rigidbody2D>().isKinematic = false;
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
         }
     }
 }
