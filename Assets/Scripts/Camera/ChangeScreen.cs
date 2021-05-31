a0e512a4748f8e98f99bac5d2211a9feb4a5fe07
Breno Dória <31598519+Breno-D@users.noreply.github.com>
2021-05-31 16:52:41 -0300

    [ADD] camera animation

--- Assets/Scripts/Camera/ChangeScreen.cs
+++ Assets/Scripts/Camera/ChangeScreen.cs
@@ -7,6 +7,7 @@ public class ChangeScreen : MonoBehaviour
 {
     [SerializeField]  CinemachineBrain p1Cam, p2Cam, bothCam;
     bool isSplit = true;
+    [SerializeField] Animator p1CanvasAnim, p2CanvasAnim, bothCanvasAnim;
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -28,9 +29,12 @@ public class ChangeScreen : MonoBehaviour
     {
         if(isSplit)
         {
-            p1Cam.gameObject.SetActive(false);
-            p2Cam.gameObject.SetActive(false);
-            bothCam.gameObject.SetActive(true);
+            // p1Cam.gameObject.SetActive(false);
+            // p2Cam.gameObject.SetActive(false);
+            // bothCam.gameObject.SetActive(true);
+            p1CanvasAnim.SetTrigger("juntar");
+            p2CanvasAnim.SetTrigger("juntar");
+            Invoke("Juntar", 0.16f);
             isSplit = !isSplit;
         }
         else
@@ -38,7 +42,16 @@ public class ChangeScreen : MonoBehaviour
             p1Cam.gameObject.SetActive(true);
             p2Cam.gameObject.SetActive(true);
             bothCam.gameObject.SetActive(false);
+            p1CanvasAnim.SetTrigger("separar");
+            p2CanvasAnim.SetTrigger("separar");
             isSplit = !isSplit;
         }
+    }
+
+    void Juntar()
+    {
+        p1Cam.gameObject.SetActive(false);
+        p2Cam.gameObject.SetActive(false);
+        bothCam.gameObject.SetActive(true);
     }
 }
